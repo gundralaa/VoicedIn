699b52226af3ca91fa621f89a17cbc1f7c1e445a
gundralaa <mathgame99@gmail.com>
2018-07-25 03:31:15 -0700

    UI fixes

--- app/src/main/java/com/example/android/voicedin/VoiceActivity.java
+++ app/src/main/java/com/example/android/voicedin/VoiceActivity.java
@@ -1,11 +1,13 @@
 package com.example.android.voicedin;
 
+import android.media.Image;
 import android.os.CountDownTimer;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
