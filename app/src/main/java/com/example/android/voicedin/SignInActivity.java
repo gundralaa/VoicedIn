492db5cf38b78c9c8d97736e0a0d27608567a120
gundralaa <mathgame99@gmail.com>
2018-07-25 04:07:49 -0700

    intro activity

--- app/src/main/java/com/example/android/voicedin/SignInActivity.java
+++ app/src/main/java/com/example/android/voicedin/SignInActivity.java
@@ -1,13 +1,31 @@
 package com.example.android.voicedin;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    ImageView view;
+    Activity context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        view = (ImageView) findViewById(R.id.introimage);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startVoice = new Intent(context, VoiceActivity.class);
+                startActivity(startVoice);
+            }
+        });
+
     }
 }
