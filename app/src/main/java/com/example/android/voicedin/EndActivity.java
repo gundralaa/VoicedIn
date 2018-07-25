70417b2652c4f75be5c7889809d2477d09d29985
Virginia Qian <vqian@live.com>
2018-07-25 08:57:31 -0700

    Parsed conversation analytics

--- app/src/main/java/com/example/android/voicedin/EndActivity.java
+++ app/src/main/java/com/example/android/voicedin/EndActivity.java
@@ -3,6 +3,7 @@ package com.example.android.voicedin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ import com.example.android.voicedin.ConvAnalyticsUtils.SentimentAnalysisResult;
 
 public class EndActivity extends AppCompatActivity {
     private static String transcript;
+    private static String conversationAnalysis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +21,8 @@ public class EndActivity extends AppCompatActivity {
         Intent startAct = getIntent();
 
         transcript = startAct.getStringExtra("Transcript");
-
-        ((EditText)findViewById(R.id.transcript)).setText(transcript);
+        ((TextView)findViewById(R.id.transcript)).setText(transcript);
+        ((TextView)findViewById(R.id.transcript)).setMovementMethod(new ScrollingMovementMethod());
 
         SentimentAnalysisResult result = new SentimentAnalysisResult(transcript);
         result.runSentimentAnalysis();
@@ -31,7 +33,11 @@ public class EndActivity extends AppCompatActivity {
         } catch (InterruptedException ie) {
             System.out.println(ie.getMessage());
         }
-        ((EditText)findViewById(R.id.conversationAnalysis)).setText(result.SentimentScore);
+
+        conversationAnalysis += "Sentiment Analysis: " + (Double.parseDouble(result.SentimentScore) * 100) + "% positive\n";
+        conversationAnalysis += "Keywords: ";
+        ((TextView)findViewById(R.id.conversationAnalysis)).setText(conversationAnalysis);
+        ((TextView)findViewById(R.id.conversationAnalysis)).setMovementMethod(new ScrollingMovementMethod());
     }
 
     public static void setTranscript(String transcript){
